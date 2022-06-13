43467d3b069ada7e34e4db08435caaaf982ceea3
Danish Shafi <danish.shafi@live.co.uk>
2022-06-13 16:51:07 +0100

    Initial flex structure created

--- projects/project0 - business_card/src/components/Card/Profile/Profile.js
+++ projects/project0 - business_card/src/components/Card/Profile/Profile.js
@@ -1,14 +1,16 @@
+import './Profile.css';
+
 const Profile = (props) => {
   return (
     <div className="card--profile">
       <img
-        className="test"
-        src="https://www.pennmedicine.org/-/media/mpd/providers/small_roy.jpg"
-        alt="pic"
+        className="card--profile-image"
+        src="https://images.squarespace-cdn.com/content/v1/5215f8e7e4b04942a67a3c8f/1589213258778-94RRWW77T6CZEK3WF0ZS/Corporate+linkedin+headshots+London-1.jpg?format=2500w"
+        alt="mark-zuckerberg"
       />
-      <h1>Bob Malibu</h1>
-      <h3>Software VP</h3>
-      <p>Available For Mentoring</p>
+      <h1>John Doe</h1>
+      <h3>VP of Software Systems</h3>
+      <p>Softex Inc.</p>
     </div>
   )
 }
--- projects/project0 - business_card/src/components/Card/Socials/Socials.js
+++ projects/project0 - business_card/src/components/Card/Socials/Socials.js
@@ -3,15 +3,15 @@ import TwitterLogo from '../../../images/twitter-3-64.png'
 import FacebookLogo from '../../../images/facebook-3-64.png'
 import InstagramLogo from '../../../images/instagram-3-64.png'
 import SnapchatLogo from '../../../images/snapchat-64.png'
-
+import './Socials.css';
 
 const Socials = (props) => {
   return (
     <div className="social--container">
-      <img src={TwitterLogo} alt="twitter" />
-      <img src={FacebookLogo} alt="twitter" />
-      <img src={InstagramLogo} alt="twitter" />
-      <img src={SnapchatLogo} alt="twitter" />
+      <a href="http://www.twitter.com"><img className="social--container-imgsize" src={TwitterLogo} alt="twitter" /></a>
+      <a href="http://www.facebook.com"><img className="social--container-imgsize" src={FacebookLogo} alt="facebook" /></a>
+      <a href="http://www.instagram.com"><img className="social--container-imgsize" src={InstagramLogo} alt="instagram" /></a>
+      <a href="http://www.snapchat.com"><img className="social--container-imgsize" src={SnapchatLogo} alt="snapchat" /></a>
     </div>
   )
 }
